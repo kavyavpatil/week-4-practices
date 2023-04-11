a410331fbc9c4e4e4914ecb44bba68fb25da95db
Kavya V Patil <103949929+kavyavpatil@users.noreply.github.com>
2023-04-11 23:55:49 +0530

    Add files via upload

---
+++ Frequency.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Frequency
+{
+public static void main(String[] args)
+{
+aScanner s=new Scanner(System.in);
+System.out.println("enetr the string");
+String str=s.nextLine();
+char arr[]=str.toCharArray();
+for(int i=0;i<arr.length;i++)
+{
+System.out.println("char is " +arr[i]);
+count++;
+}
+{
+ System.out.println("total count is  " +count);   
+}
+}
+}
